0aa13ab8b9103b73f77cf4a6f7066bfcbd9befe6
Jimson-Botterfly <opeyemiabdulateef406@gmail.com>
2022-11-21 22:22:11 +0100

    remove nodedules

--- app.js
+++ app.js
@@ -1,33 +1,45 @@
+require("dotenv").config();
 const express = require("express");
 const https = require("https");
 const bodyParser = require("body-parser");
+const ejs = require("ejs");
 
 const app = express();
+app.set("view engine", "ejs");
 
 app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.static("public"));
 
 app.get("/", function(req, res){
-res.sendFile(__dirname + "/index2.html"); 
+res.render("home", {
+    weatherData: null
+}); 
     
 })
 
 app.post("/", function(req, res){
 const query = req.body.cityName;
-    const apiKey = "f7fc5ce1d2fe0896e666feb60008bc97";
+    const apiKey = process.env.API_KEY;
     const unit = "metric";
     const url = "https://api.openweathermap.org/data/2.5/weather?q=" + query + "&appid=" + apiKey + "&units=" + unit +"";
     https.get(url, function(response){
-        console.log(response.statusCode);
+        // console.log(response.statusCode);
         response.on("data", function(data){
             const weatherData = JSON.parse(data)
-            const temp = weatherData.main.temp
-            const weatherDescription = weatherData.weather[0].description;
-            const icon = weatherData.weather[0].icon
-            const imageURL = "http://openweathermap.org/img/wn/" + icon + "@2x.png"
-            res.write("<p>The weather is currently " + weatherDescription + "</p>")
-            res.write("<h1>The temperature in " + query + " is " + temp + " degrees celcius.</h1>")
-            res.write("<img src=" + imageURL + ">");
-            res.send()
+            // const temp = weatherData.main.temp
+            // const weatherDescription = weatherData.weather[0].description;
+            // const icon = weatherData.weather[0].icon
+            // const imageURL = "http://openweathermap.org/img/wn/" + icon + "@2x.png"
+
+            res.render("home", {
+                weatherData: weatherData,
+                query: query
+            })
+
+            // res.write("<p>The weather is currently " + weatherDescription + "</p>")
+            // res.write("<h1>The temperature in " + query + " is " + temp + " degrees celcius.</h1>")
+            // res.write("<img src=" + imageURL + ">");
+            // res.send()
     
         })
          
